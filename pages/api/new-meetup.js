dfed16f975f037db77475a6641c2db496a0f1ae1
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-18 17:17:39 +0700

    importing mongodb

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+	if (req.method === "POST") {
+		const data = req.body;
+
+		const { title, image, address, description } = data;
+		const client = await MongoClient.connect(
+			`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.mceon.mongodb.net/meetups?retryWrites=true&w=majority`
+		);
+		const db = client.db();
+
+		const meetupsCollection = db.collection("meetups");
+
+		const result = await meetupsCollection.insertOne(data);
+
+		console.log(result);
+
+		client.close();
+
+		res.status(201).json.stringify({ meessage: "Meetup inserted!" });
+	}
+}
+
+export default handler;
